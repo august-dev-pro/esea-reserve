7cba74b0ae4c9bbdf4ccb52c31f75229f8c795db
august-dev-pro <augustinselete01@gmail.com>
2024-07-29 09:42:16 +0000

    update home page and begening services-overviews-page by slug

--- src/app/services/by-slug/[slug]/page.tsx
+++ src/app/services/by-slug/[slug]/page.tsx
@@ -3,10 +3,21 @@ import { services } from "@/ui/testDatas";
 import React from "react";
 
 const Page = ({ params }: { params: { slug: string } }) => {
-  const slug = params.slug.toLowerCase();
-  const service = services.find(
-    (service: any) => service.title.toLowerCase() === slug
-  );
+  const slug = typeof params.slug === "string" ? params.slug.toLowerCase() : "";
+  const service = services.find((service: any) => {
+    if (typeof service.title === "string") {
+      return service.title.toLowerCase() === slug;
+    }
+    return false;
+  });
+  if (!service) {
+    return (
+      <div className="text-center text-red-500">
+        <h2>Service introuvable</h2>
+        <p>Le service que vous cherchez n'existe pas ou a été supprimé.</p>
+      </div>
+    );
+  }
 
   return (
     <div className="">
