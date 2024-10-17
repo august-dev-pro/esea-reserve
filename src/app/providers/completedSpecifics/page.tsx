b83d23f1f976a3f1efc7ee08dc3ffb591d116430
august-dev-pro <augustinselete01@gmail.com>
2024-10-17 01:33:11 +0000

    prevent test version of easyreserve version 1.01

--- src/app/providers/completedSpecifics/page.tsx
+++ src/app/providers/completedSpecifics/page.tsx
@@ -2,7 +2,7 @@
 import RegisterTaskerSpecificsForm from "@/components/forms/RegisterTaskerSpecificsForm";
 import React from "react";
 
-const page = () => {
+const Page = () => {
   const handleStartRegister = () => {};
   return (
     <div>
@@ -11,4 +11,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
